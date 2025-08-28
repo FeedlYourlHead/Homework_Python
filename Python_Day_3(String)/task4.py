2f6624a95d7a265ad896459948ef0ac88906cbe2
Denis Markov <da.marckov2015@yandex.ru>
2025-08-28 16:07:22 +0300

    add ascii library

--- Python_Day_3(String)/task4.py
+++ Python_Day_3(String)/task4.py
@@ -1,9 +1,14 @@
 from random import randint
+from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits,
+
 num = int(input("Введите длину вашего пароля: "))
-result = ""
-for i in range(num):
-    num = randint(48, 122)
-    char = chr(num)
-    result += char
-print(f"Ваш пароль: {result}")
+if num < 4:
+    print("Пароль должен быть больше 4 символов")
+else:
+    result = ""
+    for i in range(num):
+        num = randint(48, 122)
+        char = chr(num)
+        result += char
+    print(ascii_lowercase)
 
