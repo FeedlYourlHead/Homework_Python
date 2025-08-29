3847387e6ebda69098de6f131b403edd8c17d870
Denis Markov <da.marckov2015@yandex.ru>
2025-08-29 17:27:52 +0700

    add img folder

--- Python_Day_3(String)/task4.py
+++ Python_Day_3(String)/task4.py
@@ -1,14 +1,10 @@
-from random import randint
-from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits,
+from random import randint, shuffle
+from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
 
 num = int(input("Введите длину вашего пароля: "))
 if num < 4:
     print("Пароль должен быть больше 4 символов")
 else:
     result = ""
-    for i in range(num):
-        num = randint(48, 122)
-        char = chr(num)
-        result += char
-    print(ascii_lowercase)
+    
 
