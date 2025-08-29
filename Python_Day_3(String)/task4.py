56201464c637fb023d7ae7d91408726e448d707d
Denis Markov <da.marckov2015@yandex.ru>
2025-08-29 22:26:35 +0700

    add try except

--- Python_Day_3(String)/task4.py
+++ Python_Day_3(String)/task4.py
@@ -1,30 +1,36 @@
 from random import shuffle, choice
 from string import ascii_lowercase, ascii_uppercase, digits
 
-num = int(input("Введите длину вашего пароля: "))
-if num < 4:
-    print("Пароль должен быть больше 4 символов")
-else:
-    result = []
-    symbols = '!@#$%^&*()'
-    count = 0
-    while count < num:
-        result.append(choice(ascii_lowercase))
-        count += 1
-        if count == num:
+symbols = '!@#$%^&*()'
+while True:
+    try:
+        num = int(input("Введите длину вашего пароля: "))
+        if num < 4:
+            print("Пароль должен быть больше 4 символов")
+            continue
+        else:
+            result = []
+            count = 0
+            while count < num:
+                result.append(choice(ascii_lowercase))
+                count += 1
+                if count == num:
+                    break
+                result.append(choice(ascii_uppercase))
+                count += 1
+                if count == num:
+                    break
+                result.append(choice(digits))
+                count += 1
+                if count == num:
+                    break
+                result.append(choice(symbols))
+                count += 1
             break
-        result.append(choice(ascii_uppercase))
-        count += 1
-        if count == num:
-            break
-        result.append(choice(digits))
-        count += 1
-        if count == num:
-            break
-        result.append(choice(symbols))
-        count += 1
+    except Exception:
+        print("Введите целое число!!!")
 shuffle(result)
 result = ''.join([str(x) for x in result])
-print(result)
+print(f"Ваш пароль: {result}")
     
 
