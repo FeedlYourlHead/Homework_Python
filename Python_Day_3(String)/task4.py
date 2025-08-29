ce16f11a4c19a49fcf5601afe530d7861cc3c04f
Denis Markov <da.marckov2015@yandex.ru>
2025-08-29 19:33:58 +0700

    finish task4

--- Python_Day_3(String)/task4.py
+++ Python_Day_3(String)/task4.py
@@ -1,10 +1,30 @@
-from random import randint, shuffle
-from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits
+from random import randint, shuffle, choice
+from string import ascii_lowercase, ascii_uppercase, digits
 
 num = int(input("Введите длину вашего пароля: "))
 if num < 4:
     print("Пароль должен быть больше 4 символов")
 else:
-    result = ""
+    result = []
+    symbols = '!@#$%^&*()'
+    count = 0
+    while count < num:
+        result.append(choice(ascii_lowercase))
+        count += 1
+        if count == num:
+            break
+        result.append(choice(ascii_uppercase))
+        count += 1
+        if count == num:
+            break
+        result.append(choice(digits))
+        count += 1
+        if count == num:
+            break
+        result.append(choice(symbols))
+        count += 1
+shuffle(result)
+result = ''.join([str(x) for x in result])
+print(result)
     
 
