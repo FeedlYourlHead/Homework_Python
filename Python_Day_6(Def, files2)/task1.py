d66efea892e0af5bbaefd7a7b101b65104cc09ca
Denis Markov <da.marckov2015@yandex.ru>
2025-09-14 13:50:48 +0700

    add comments

--- Python_Day_6(Def, files2)/task1.py
+++ Python_Day_6(Def, files2)/task1.py
@@ -1,21 +1,27 @@
+stop_words = ["и", "в", "на", "с"]
 
-stop_words = ['и', 'в', 'на', 'с']
 
 def word_count(filename):
-    with open(f"{filename}", "r", encoding='utf-8') as f:
-        return len(f.read().split(' '))
+    """Функция, которая считает количество слов, в файле"""
+    with open(f"{filename}", "r", encoding="utf-8") as f:
+        return len(f.read().split(" "))
+
 
 def find_word(filename, word):
+    """Функция, которая ищет слово"""
     count = 0
-    with open(f"{filename}", 'r', encoding='utf-8') as f:
-        for i in f.read().split(' '):
+    with open(f"{filename}", "r", encoding="utf-8") as f:
+        for i in f.read().split(" "):
             if i == word:
                 count += 1
         return count
 
 
 def save_statistics(filename):
+    """Функция, которая сохраняет статистику по словам в файле"""
     pass
 
-print(word_count('book.txt'))
-print(find_word('book.txt', 'Франция'))
+
+print(word_count("book.txt"))
+print(find_word("book.txt", "Франция"))
+
