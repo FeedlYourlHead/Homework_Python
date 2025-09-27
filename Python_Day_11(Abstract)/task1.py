5295bde0e5077af75091cbd17ed3d8d5504e56f8
Denis Markov <da.marckov2015@yandex.ru>
2025-09-27 23:14:22 +0700

    do some work

--- Python_Day_11(Abstract)/task1.py
+++ Python_Day_11(Abstract)/task1.py
@@ -6,7 +6,31 @@ class Employee(ABC):
         self.role = role
 
     @abstractmethod
-    def work(self):
+    def work(self, task) -> str:
+        """Функция, которая возвращает результат работы работника"""
         pass
 
+class Developer(Employee):
+    def work(self, task):
+        return f"Разрабатываю программу"
 
+class Tester(Employee):
+    def work(self, task):
+        return f"Тестирую программу"
+
+class Manager(Employee):
+    def work(self, task):
+        return f"Управляю командой"
+
+class LeadDeveloper(Developer, Manager):
+    pass
+
+class Task:
+    def __init__(self, task, emloyee) -> None:
+        self.task = task
+        self.emloyee = emloyee
+
+class Project:
+    def __init__(self, tasks, emloyees) -> None:
+        self.tasks = tasks
+        self.emloyees = emloyees
