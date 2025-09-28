a186d51e81534a8b1e310149c6b110b0c6cfa18c
Denis Markov <da.marckov2015@yandex.ru>
2025-09-28 22:35:58 +0700

    add some features

--- Python_Day_11(Abstract)/task1.py
+++ Python_Day_11(Abstract)/task1.py
@@ -11,26 +11,67 @@ class Employee(ABC):
         pass
 
 class Developer(Employee):
+    def __init__(self, name):
+        super().__init__(name, role="Developer")
+
     def work(self, task):
-        return f"Разрабатываю программу"
+        task.condition -= 1
+        return  f"Работа окончена" if task.condition == 0 else f"Разрабатываю код, до окончания задания {task.name} осталось: {task.condition}"
 
 class Tester(Employee):
+    def __init__(self, name):
+        super().__init__(name, role="Tester")
     def work(self, task):
-        return f"Тестирую программу"
+        task.condition -= 1
+        return  f"Работа окончена" if task.condition == 0 else f"Тестирую код, до окончания задания {task.name} осталось: {task.condition}"
 
 class Manager(Employee):
+    def __init__(self, name):
+        super().__init__(name, role="Manager")
     def work(self, task):
-        return f"Управляю командой"
+        task.condition -= 1
+        return  f"Работа окончена" if task.condition == 0 else f"Управляю командой, до окончания задания {task.name} осталось: {task.condition}"
 
 class LeadDeveloper(Developer, Manager):
-    pass
+    def __init__(self, name, role):
+        super(Developer).__init__(name)
+        super(Manager).__init__(name, role)
 
 class Task:
     def __init__(self, task, emloyee) -> None:
         self.task = task
         self.emloyee = emloyee
+        self.condition = 3
 
 class Project:
     def __init__(self, tasks, emloyees) -> None:
         self.tasks = tasks
         self.emloyees = emloyees
+
+# dev = Developer("Boris")
+# print(dev.name)
+# print(dev.role)
+
+
+def make_project():
+    pass
+
+
+def main():
+    while True:
+        try:
+            choice = int(input("""Введите номер команды:
+            1. Создать проект.
+            2. Добавить задачу в проект.
+            3. Добавить сотрудника в проект.
+            4. Управление сотрудниками.
+            5. Выход."""))
+            if choice == 1:
+                make_project()
+            elif choice == 2:
+                pass
+            elif choice == 5:
+                break
+        except Exception:
+            pass
+
