211a62e7e79cd31d9a260d0f66e10bc8f557094b
Denis Markov <da.marckov2015@yandex.ru>
2025-10-19 14:03:39 +0700

    finish the task

--- Python_Day_14(Decor)/task1.py
+++ Python_Day_14(Decor)/task1.py
@@ -1,9 +1,23 @@
 def requires_permission(permission):
-    def wrapper():
-        pass
-    return wrapper
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            current_user_permissions = ['admin']
+            if permission in current_user_permissions:
+                return func(*args, **kwargs)
+            else:
+                return f'Ошибка: у пользователя {permission} нет прав доступа'
+        return wrapper
+    return decorator
 
 
-@requires_permission
+@requires_permission('default_user')
 def delete_user(user_id):
     return f"User {user_id} deleted"
+
+@requires_permission('admin')
+def edit_user(user_id):
+    return f'User {user_id} was edited'
+
+print(delete_user("123"))
+print(edit_user('123'))
+#Честно говоря не знаю, это ли требовалось в задании?
