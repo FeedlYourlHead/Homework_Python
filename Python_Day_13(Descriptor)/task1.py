026810ced5b1b8a48f87c6d74acbd1c5f531a5ff
Denis Markov <da.marckov2015@yandex.ru>
2025-10-19 16:06:18 +0700

    make template for task 13

---
+++ Python_Day_13(Descriptor)/task1.py
@@ -0,0 +1,20 @@
+class FinancialDescriptor:
+    pass
+
+class BalanceDescriptor(FinancialDescriptor):
+    pass
+
+class TransactionDescriptor(FinancialDescriptor):
+    pass
+
+class CategoryDescriptor(FinancialDescriptor):
+    pass
+
+
+
+
+
+class FinancialAccount:
+    pass
+
+
