e0ad81b59df26ac53e1d42e3943c20c9d3c14df0
Denis Markov <da.marckov2015@yandex.ru>
2025-11-05 15:10:13 +0700

    make stoppers

--- Python_Day_16(Decor)/task1.py
+++ Python_Day_16(Decor)/task1.py
@@ -1,4 +1,22 @@
+def cache_balance():
+    def wrapper():
+        pass
+    return wrapper
 
+def limit():
+    def wrapper():
+        pass
+    return wrapper
+
+def require_role():
+    def wrapper():
+        pass
+    return wrapper
+
+def log_transaction():
+    def wrapper():
+        pass
+    return wrapper
 
 def process_transaction(account_id:int, amount:float, transaction_type:str):
     pass
