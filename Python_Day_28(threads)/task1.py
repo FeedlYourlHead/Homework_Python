08ae455b48e56d06718ddf2940d0a812468ce84f
Denis Markov <da.marckov2015@yandex.ru>
2025-12-05 14:36:27 +0700

    refactor code

--- Python_Day_28(threads)/task1.py
+++ Python_Day_28(threads)/task1.py
@@ -8,12 +8,12 @@ def send_notification(user, delay):
 
 if __name__ == '__main__':
     users = [('Alice', 2), ('Bob', 3), ('Charlie', 1), ('Diana', 4)]
-    threads = []
+    threads = [] 
     for user in users:
         t = threading.Thread(target=send_notification, args=user)
         threads.append(t)
         t.start()
-    for t in threads:
-        t.join()
+
+    result = [t.join() for t in threads]
 
 
