2cbfa9cf3f3cd1048807b93848d0049fd9c45002
Denis Markov <da.marckov2015@yandex.ru>
2026-01-13 01:01:04 +0700

    start task

---
+++ Python_Day_33(SQL Alchemy)/main.py
@@ -0,0 +1,50 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, DateTime, Numeric, Integer, ForeignKey
+import datetime
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(30),unique=True)
+    email: Mapped[str] = mapped_column(String(30), unique=True)
+    registratin_date: Mapped[datetime.datetime.utcnow] = mapped_column(DateTime) #TODO:Исправить, тут неверно, наверное
+    orders = relationship('Order', back_populates='users')
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user: Mapped[str] = mapped_column(String(30))
+    category: Mapped[str] = mapped_column(String(30))
+    price: Mapped[float] = mapped_column(Numeric())
+    quantity_in_stock: Mapped[int] = mapped_column(Integer)
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id') )
+    order_date: Mapped[datetime.datetime.utcnow] = mapped_column(DateTime)
+    status: Mapped[str] = mapped_column(String())
+    users = relationship('User', back_populates='orders')
+
+class OrderItem(Base):
+    __tablename__ = 'order_items'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    order_id: Mapped[int] = mapped_column(Integer(), ForeignKey('orders.id'))
+    product_id: Mapped[int] = mapped_column(Integer(), ForeignKey('products.id'))
+    quantity: Mapped[int] = mapped_column(Integer())
+    price_at_order: Mapped[float] = mapped_column(Numeric(), ) #TODO: Сделать, чтобы цена товара на момент заказа, копировалась из Product.price
+
+    order: Mapped['Order'] = relationship(
+        back_populates='items'
+    )
+
+    product: Mapped["Product"] = relationship(
+        back_populates='order_items',
+        lazy='joined'
+    )
