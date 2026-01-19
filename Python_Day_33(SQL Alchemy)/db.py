63c06e7d7189148d678ca3cc8747b6453a968792
Denis Markov <da.marckov2015@yandex.ru>
2026-01-19 11:24:45 +0700

    finish db.py

--- Python_Day_33(SQL Alchemy)/db.py
+++ Python_Day_33(SQL Alchemy)/db.py
@@ -10,53 +10,65 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String, nullable=False)
-    email: Mapped[str] = mapped_column(String, nullable=False)
-    registratin_date: Mapped[datetime.datetime] = mapped_column(DateTime) #TODO:Исправить, тут неверно, наверное
-    orders = relationship('Order', back_populates='users')
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    registratin_date: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow) #TODO:Исправить, тут неверно, наверное
+    orders = relationship('Order', back_populates='users', cascade='all, delete-orphan')
 
     def __repr__(self) -> str:
         return f'id={self.id} ,username={self.username}, email={self.email}, registratin_date={self.registratin_date}'
 
 class Product(Base):
     __tablename__ = 'products'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String(30))
-    category: Mapped[str] = mapped_column(String(30))
-    price: Mapped[float] = mapped_column(Numeric())
-    quantity_in_stock: Mapped[int] = mapped_column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    category: Mapped[str] = mapped_column(String, nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+    quantity_in_stock: Mapped[int] = mapped_column(Integer, default=0)
 
     def __repr__(self) -> str:
         return f'id={self.id}, category={self.category}, price={self.price}, quantity_in_stock={self.quantity_in_stock}'
 
 class Order(Base):
     __tablename__ = 'orders'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id') )
-    order_date: Mapped[datetime.datetime] = mapped_column(DateTime)
-    status: Mapped[str] = mapped_column(String())
-    users = relationship('User', back_populates='orders')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    order_date: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
+    status: Mapped[str] = mapped_column(String, default='в обработке')
+    user = relationship(
+        'User',
+        back_populates='orders'
+    )
+    items = relationship(
+        'OrderItem',
+        back_populates='order',
+        cascade='all, delete-orphan'
+    )
+
+    def calculate_total(self):
+        return sum(item.quantity * item.price_at_order for item in self.items)
 
     def __repr__(self) -> str:
         return f'id={self.id}, order_date={self.order_date}, status={self.status} '
 
 class OrderItem(Base):
     __tablename__ = 'order_items'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    order_id: Mapped[int] = mapped_column(Integer(), ForeignKey('orders.id'))
-    product_id: Mapped[int] = mapped_column(Integer(), ForeignKey('products.id'))
-    quantity: Mapped[int] = mapped_column(Integer())
-    price_at_order: Mapped[float] = mapped_column(Numeric, ) #TODO: Сделать, чтобы цена товара на момент заказа, копировалась из Product.price
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_id: Mapped[int] = mapped_column(Integer, ForeignKey('orders.id', ondelete='CASCADE'), nullable=False)
+    product_id: Mapped[int] = mapped_column(Integer, ForeignKey('products.id'), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    price_at_order: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False) #TODO: Сделать, чтобы цена товара на момент заказа, копировалась из Product.price
+    
 
     order: Mapped['Order'] = relationship(
+        'Order',
         back_populates='items'
     )
 
     product: Mapped["Product"] = relationship(
-        back_populates='order_items',
-        lazy='joined'
+        'Product',
+        back_populates='order_items'
     )
-
 
     def __repr__(self) -> str:
         return f'id={self.id}, '
