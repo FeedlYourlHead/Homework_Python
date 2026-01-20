5fa3e17a379320dbd6417195e78646d1f1615b28
Denis Markov <da.marckov2015@yandex.ru>
2026-01-20 14:35:08 +0700

    finish the task

--- Python_Day_33(SQL Alchemy)/queries.py
+++ Python_Day_33(SQL Alchemy)/queries.py
@@ -1,4 +1,26 @@
+from seed import DatabaseManager
+from db import Product, Order, OrderItem, User
 
+
+
+if __name__ == "__main__":
+    # Смартфон <50_000
+    db_manager = DatabaseManager()
+    session = db_manager.get_session()
+
+    query_smartphone = session.query(Product).filter(Product.price < 50_000, Product.category == "Смартфон").all()
+
+    for smartphone in query_smartphone:
+        print(f'id={smartphone.id}\nname={smartphone.name}\ncategory={smartphone.category}\nprice={smartphone.price}\nquantity_in_stock={smartphone.quantity_in_stock}\n')
+
+    #  Join таблиц по email, список заказов примера ivan@yandex.ru
+    email_target = 'ivan@yandex.ru'
+    quer = session.query(Order).join(User).filter(User.email == email_target).all()
+    for elem in quer:
+        print(f'Дата заказа - {elem.order_date}, статус - {elem.status}')
+
+
+    
 
 
 
