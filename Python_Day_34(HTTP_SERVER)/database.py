3bccb0a363ba107ec9ea8f72914fee3a5ced1580
Denis Markov <da.marckov2015@yandex.ru>
2026-01-23 21:33:21 +0700

    add db

--- Python_Day_34(HTTP_SERVER)/database.py
+++ Python_Day_34(HTTP_SERVER)/database.py
@@ -1,3 +1,22 @@
-import sqlalchemy.orm 
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, DateTime, create_engine
+import datetime
 
+
+class Base(DeclarativeBase):
+    pass
+
+class Message(Base):
+    __tablename__ = "messages"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[str] = mapped_column(String)
+    content: Mapped[str] = mapped_column(String)
+    unlock_date: Mapped[datetime.datetime] = mapped_column(DateTime)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
+
+DB_URL = 'sqlite:///messages.db'
+engine = create_engine(DB_URL, echo=True)
+
+def set_message():
+    pass
 
