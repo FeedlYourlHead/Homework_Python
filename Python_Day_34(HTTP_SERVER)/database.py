da51e91fc6ee6f471c92969794d9a9c9f2e6de04
Denis Markov <da.marckov2015@yandex.ru>
2026-01-26 14:08:55 +0700

    refactor some code

--- Python_Day_34(HTTP_SERVER)/database.py
+++ Python_Day_34(HTTP_SERVER)/database.py
@@ -1,35 +1,21 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
-from sqlalchemy import String, DateTime, create_engine, select
+from flask_sqlalchemy import SQLAlchemy
 import datetime
 
+db = SQLAlchemy()
 
-class Base(DeclarativeBase):
-    pass
 
-class Message(Base):
+class Message(db.Model):
     __tablename__ = "messages"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    author: Mapped[str] = mapped_column(String)
-    content: Mapped[str] = mapped_column(String)
-    unlock_date: Mapped[datetime.datetime] = mapped_column(DateTime)
-    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
 
-DB_URL = 'sqlite:///messages.db'
-engine = create_engine(DB_URL, echo=True)
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    unlock_date = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
-def setup_database():
-    Base.metadata.create_all(engine)
 
-def get_all_messages():
-    with Session(engine) as session:
-        select_messages = select(Message).order_by(Message.content)
-        messages = session.scalars(select_messages).all()
-        return messages
+    def is_unlocked(self):
+        return datetime.datetime.utcnow().date() >= self.unlock_date
 
-def get_all_authors():
-    with Session(engine) as session:
-        select_authors = select(Message).order_by(Message.author)
-        authors = session.scalars(select_authors).all()
-        return authors
 
 
