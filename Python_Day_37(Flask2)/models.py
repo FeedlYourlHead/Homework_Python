5e1069de798b9398565ddb8f4ef8df885e443963
Denis Markov <da.marckov2015@yandex.ru>
2026-02-22 00:21:37 +0700

    add some files

---
+++ Python_Day_37(Flask2)/models.py
@@ -0,0 +1,13 @@
+from flask_login import UserMixin
+from .extensions import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password_hash = db.Column(db.String(30), nullable=False)
+
+
+
+
+
+
